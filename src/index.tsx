40a619c3899b9caa246e2408bcbeaf183b5ae9b9
Wallace Antunes <wallace.antunes@ubiqui.com.br>
2021-06-28 17:33:52 -0300

    adjusting store and api request

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/scss/global.scss'
 import { App } from './App';
-
+import { Provider } from 'react-redux'
+import store from './store/index'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
